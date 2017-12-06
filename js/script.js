85aa0edaa65deff29556a7c18aeec1daab18a4e0
Rituj Kumar <ritujkumar@hotmail.com>
2017-12-06 13:17:53 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function addition() {
+  var a = parseInt(document.getElementById('input1').value);
+  var b = parseInt(document.getElementById('input2').value);
+
+  var c = a + b;
+  console.log(c);
+  alert(c);
+}
+
+function subtraction() {
+  var a = parseInt(document.getElementById('input1').value);
+  var b = parseInt(document.getElementById('input2').value);
+
+  var c = a - b;
+  console.log(c);
+  alert(c);
+}
+
+function multiplication() {
+  var a = parseInt(document.getElementById('input1').value);
+  var b = parseInt(document.getElementById('input2').value);
+
+  var c = a * b;
+  console.log(c);
+  alert(c);
+}
+
+function division() {
+  var a = parseInt(document.getElementById('input1').value);
+  var b = parseInt(document.getElementById('input2').value);
+
+  var c = a / b;
+  console.log(c);
+  alert(c);
+}
